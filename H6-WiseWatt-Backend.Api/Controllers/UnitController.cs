173ae2af31e30467905355308598ce16f7206a8a
anit3k <steffen.halberg@gmail.com>
2024-04-18 13:06:59 +0200

    added contorller

---
+++ H6-WiseWatt-Backend.Api/Controllers/UnitController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace H6_WiseWatt_Backend.Api.Controllers
+{
+    [ApiController]
+    public class UnitController : ControllerBase
+    {
+        public UnitController()
+        {
+            
+        }
+        #region GetAllUsersDevices
+        [HttpGet]
+        [Route("api/device/state")]
+        public async Task<IActionResult> GetState()
+        {
+            try
+            {
+               
+                return Ok("test");
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"An error has occurred with error message: {ex.Message}");
+                return StatusCode(statusCode: 500, "Something went wrong please contact your administrator");
+            }
+        }
+        #endregion
+    }
+}
