00c25ece1f8793954dddbcf92e001bed1bf36352
anit3k <steffen.halberg@gmail.com>
2024-04-24 15:00:27 +0200

    Updated price service test

---
+++ H6-WiseWatt-Backend.Test/ElectricPriceServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using H6_WiseWatt_Backend.Domain.Services;
+using H6_WiseWatt_Backend.Domain.Entities;
+using H6_WiseWatt_Backend.Domain.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace H6_WiseWatt_Backend.Tests
+{
+    [TestFixture]
+    public class ElectricPriceServiceTests
+    {
+        private Mock<IElectricityPriceRepo> _mockPriceRepo;
+        private Mock<HttpClient> _mockHttpClient;
+        private ElectricPriceService _electricPriceService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockPriceRepo = new Mock<IElectricityPriceRepo>();
+            _mockHttpClient = new Mock<HttpClient>();
+            _electricPriceService = new ElectricPriceService(_mockPriceRepo.Object, _mockHttpClient.Object);
+        }
+
+        [Test]
+        public async Task GetElectricityPricesAsync_ShouldFetchPricesIfNoneAvailable()
+        {
+            // Arrange
+            _mockPriceRepo.Setup(r => r.GetAllPrices()).ReturnsAsync(new List<ElectricityPriceEntity>());
+            _mockPriceRepo.Setup(r => r.UpdatePrices(It.IsAny<List<ElectricityPriceEntity>>())).Returns(Task.CompletedTask);
+
+            // Act
+            var prices = await _electricPriceService.GetElectricityPricesAsync();
+
+            // Assert
+            Assert.IsNotNull(prices);
+            _mockPriceRepo.Verify(r => r.UpdatePrices(It.IsAny<List<ElectricityPriceEntity>>()), Times.Once);
+        }       
+    }
+}
